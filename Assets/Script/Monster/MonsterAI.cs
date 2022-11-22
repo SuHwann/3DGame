f5a42895180781dfd395837bd89424e23233b746
Leesuhwa <dltnghks3256@gmail.com>
2022-11-22 11:55:21 +0900

    ㅇㅇ

--- Assets/Script/Monster/MonsterAI.cs
+++ Assets/Script/Monster/MonsterAI.cs
@@ -2,20 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using System;
 public class MonsterAI : MonoBehaviour
 {
     [SerializeField]
+    private int maxHealth; //최대 체력
+    [SerializeField]
+    private int curHealth; //현재 체력 
+    [SerializeField]
     Transform[] movePoint;    //몬스터 목표 위치 변수
+    [SerializeField]
+    private bool isChase = false;  //Player를 추적을 결정하는 변수 
+    [SerializeField]
+    GameObject target;    //Player Target
     NavMeshAgent agent;       //NMA 변수
     Vector3 dePosition;       //기존 위치 
     int randomInt;            //movePoint 순서를 랜덤하게 저장 할 변수
     Rigidbody rigid;          //충돌시 일어나는 예외 상황 방지 변수
+    BoxCollider boxCollider;
+    Material mat;
+    Animator anim;
+    IEnumerator ai;
     private void Awake()
     {
         GetPoint();
         VariableRest();
-        randomInt = Random.Range(0, movePoint.Length); //랜덤 변수 저장
+        randomInt = UnityEngine.Random.Range(0, movePoint.Length); //랜덤 변수 저장
         dePosition = transform.position; //첫시작시 위치 저장
+        ai = AiMonster();
         StartCoroutine(AiMonster());
     }
     void GetPoint()  //Point들의 위치를 인스펙터에 저장한다.
@@ -26,27 +40,98 @@ public class MonsterAI : MonoBehaviour
     {
         while(true)
         {
-            agent.SetDestination(movePoint[randomInt].transform.position); //ai몬스터 목적지로 이동 시작
-            FreezeVelocity();
-            Destination();
-            yield return new WaitForSeconds(Time.deltaTime);
+            if(!isChase)
+            {
+                agent.SetDestination(movePoint[randomInt].transform.position); //ai몬스터 목적지로 이동 시작
+                FreezeVelocity();
+                Destination();
+                yield return new WaitForSeconds(Time.deltaTime);
+            }
+        }
+    }
+    private void Update()
+    {
+        //Player와 자기 자신이 거리가 3f 보다 작으면  Player를 추적한다.
+        if (Vector3.Distance(transform.position , target.transform.position)<3f)
+        {
+            ChaseStart();
+            agent.SetDestination(target.transform.position);
         }
     }
     void VariableRest() //변수 초기화.
     {
         agent = GetComponent<NavMeshAgent>();
         rigid = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+        mat = GetComponentInChildren<SkinnedMeshRenderer>().material;    //Material은 Mesh Renderer 컴포넌트에서 접근가능!
+        anim = GetComponent<Animator>();
     }
     void FreezeVelocity()   //ai 몬스터 충돌시 뒤로 밀리는 충돌 멈춤 
     {
-        rigid.velocity = Vector3.zero;
-        rigid.angularVelocity = Vector3.zero;
+        if(isChase)
+        {
+            rigid.velocity = Vector3.zero;
+            rigid.angularVelocity = Vector3.zero;
+        }
     }
     void Destination() //목적지에 도착하면 목적지 변경
     {
         if (Vector3.Distance(transform.position, movePoint[randomInt].transform.position) < 1f)
         {
-            randomInt = Random.Range(0, movePoint.Length);
+            randomInt = UnityEngine.Random.Range(0, movePoint.Length);
+        }
+    }
+    //Walk Anim 스타트 함수
+    void ChaseStart()
+    {
+        isChase = true;
+        anim.SetBool("isWalk", true);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //근거리 공격일때 
+        if (other.tag == "Melee")
+        {
+            Weapon weapon = other.GetComponent<Weapon>();
+            curHealth -= weapon.damage;
+            //넉백 위치 or 전달
+            Vector3 reactVec = transform.position - other.transform.position;
+            StartCoroutine(OnDamage(reactVec));
+        }
+        //원거리 공격일때 
+        /*        else if(other.tag == "Bullet")
+                {
+                    Bullet bullet = other.GetComponent<Bullet>();
+                    curHealth -= bullet.damage;
+
+                    StartCoroutine(OnDamage(reactVec));
+                }*/
+    }
+
+    //데미지 로직 
+    IEnumerator OnDamage(Vector3 reactVec)
+    {
+        mat.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        //몬스터 살아있음 
+        if (curHealth > 0)
+        {
+            mat.color = Color.white;
+        }
+        //몬스터 죽음 
+        else
+        {
+            mat.color = Color.gray;
+            gameObject.layer = 7;
+            //몬스터가 죽는 시점에 애니메이션과 플래그 셋팅
+            StopCoroutine(ai);
+            agent.enabled = false;
+            anim.SetTrigger("doDie");
+            //몬스터가 죽으면서 뒤로 밀림
+            reactVec = reactVec.normalized;
+            reactVec += Vector3.up;
+            rigid.AddForce(reactVec * 5, ForceMode.Impulse);
+            Destroy(gameObject, 2f);
         }
     }
 }
