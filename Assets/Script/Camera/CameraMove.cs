02bd2c608e79123934ee7c2df4bd2ab3c283be91
Leesuhwa <dltnghks3256@gmail.com>
2022-11-30 17:57:45 +0900

    3인칭 카메라 만들기 실패 ㅜ
    집가서 해야징

--- Assets/Script/Camera/CameraMove.cs
+++ Assets/Script/Camera/CameraMove.cs
@@ -5,13 +5,62 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    [SerializeField]
+    public Transform objectTofollw; //따라가야할 오브젝트 위치 정보
+    public float followSpeed = 10f;       //따라갈 스피드
+    public float sensitivity = 100f;        //마우스 감도 
+    public float clampAngle = 70f;  //마우스를 움직때 상수 각도 
+    //마우스 Input을 받을 정보
+    private float rotx;
+    private float roty;
+
+    public Transform realCamera;        //카메라의 정보
+    public Vector3 dirNormalized;       //카메라 방향 
+    public Vector3 finalDir;            //최종적으로 정해진 방향
+    public float minDistance;           //최소 거리 
+    public float maxDistance;           //최대 거리 
+    public float finalDistatnce;        //최종 거리 
+    public float smoothness = 10f;
+    private void Start() //변수 초기 화
+    {
+        rotx = transform.localRotation.eulerAngles.x;
+        roty = transform.localRotation.eulerAngles.y;
+
+        dirNormalized = realCamera.localPosition.normalized;
+        finalDistatnce = realCamera.localPosition.magnitude;
+    }
+    private void Update()
+    {
+        rotx += -(Input.GetAxis("Mouse Y")) * sensitivity * Time.deltaTime; //rotx인데 왜 y를 input을 받을까? y축 방향으로 좌우로 움직이기 때문
+        roty += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+
+        rotx = Mathf.Clamp(rotx,-clampAngle , clampAngle);              //수학함수 쓸때 Mathf를 사용
+        Quaternion rot = Quaternion.Euler(rotx, roty, 0);
+        transform.rotation = rot;
+    }
+    private void LateUpdate() //Update가 끝난 다음에 실행되는 함수이다.
+    {
+        transform.position = Vector3.MoveTowards(transform.position, objectTofollw.position,
+        followSpeed * Time.deltaTime);
+        finalDir = transform.TransformPoint(dirNormalized * maxDistance);
+
+        RaycastHit hit;
+
+        if(Physics.Linecast(transform.position,finalDir,out hit))
+        {
+            finalDistatnce = Mathf.Clamp(hit.distance, minDistance, maxDistance);
+        }
+        else
+        {
+            finalDistatnce = maxDistance;
+        }
+        realCamera.localPosition = Vector3.Lerp(realCamera.localPosition, dirNormalized * finalDistatnce, Time.deltaTime * smoothness);
+            
+    }
+}
+/*    [SerializeField]
     Transform target;
     //카메라 vector변수
     private Vector3 offset;
-
-    public float xmove = 2; // X축 누적 이동량 
-    public float ymove = 25; // Y축 누적 이동량
     private void Awake()
     {
         //카메라 자기 자신 transform위치 값을 offset에 저장
@@ -21,13 +70,5 @@ public class CameraMove : MonoBehaviour
     {
         //카메라가 tartget을 따라가게끔 설정
         transform.position = target.position + offset;
-        if (Input.GetMouseButton(1))
-        {
-            xmove += Input.GetAxis("Mouse X");
-            // 마우스의 좌우 이동량을 xmove 에 누적합니다. 
-            ymove -= Input.GetAxis("Mouse Y");
-            // 마우스의 상하 이동량을 ymove 에 누적합니다. 
-        }
     }
-
-}
+*/
